32648db0ef811107f09360402715104dfef0ae66
MKTripsy <janchristiansentripolca@gmail.com>
2024-10-11 15:38:04 +0800

    dovunque menu

--- data/products.js
+++ data/products.js
@@ -1,7 +1,28 @@
 const products = [
-    { id: 1, name: 'Espresso', price: 5, description: 'Rich and bold espresso', image: 'espresso.jpg' },
-    { id: 2, name: 'Cappuccino', price: 6, description: 'Creamy cappuccino', image: 'cappuccino.jpg' },
-    { id: 3, name: 'Latte', price: 5.5, description: 'Smooth latte with milk', image: 'latte.jpg' },
+
+    { id: 1, name: 'Fried Mozzarella', price: 350.00, description: 'Breaded and Deep Fried Mozzarella Cheese', image: 'espresso.jpg' },
+    { id: 2, name: 'Fried Calamari', price: 450.00, description: 'Breaded and Deep Fried Squeed', image: 'cappuccino.jpg' },
+    { id: 3, name: 'Chicken Alfredo', price: 600.00, description: 'Chicken Cutlet and Alfredo Sauce on Fetuccine Pasta', image: 'latte.jpg' },
+    { id: 4, name: 'Fetuccine Alfredo', price: 500.00, description: 'Classic Alfredo Sauce on Fetuccine Pasta', image: 'latte.jpg' },
+    { id: 5, name: 'Shrimp Alfredo', price: 675.00, description: 'Shrimp and Alfredo Sauce on Fetuccine Pasta', image: 'latte.jpg' },
+    { id: 6, name: 'Chicken Carbonara', price: 650.00, description: 'Chicken Cutlet and Creamy Carbonara Sauce on Fetuccine Pasta', image: 'latte.jpg' },
+    { id: 7, name: 'Classic Carbonara', price: 550.00, description: 'Creamy Carbonara Sauce on Fetuccine Pasta', image: 'latte.jpg' },
+    { id: 8, name: 'Chicken Parmesan', price: 650.00, description: 'Chicken Cutlet and Marinara Sauce on Fetuccine Pasta', image: 'latte.jpg' },
+    { id: 9, name: 'Chicken Scampi', price: 650.00, description: 'Sliced Chicken Cutlet with garlic butter and white wine reduction on Fetuccine Pasta', image: 'latte.jpg' },
+    { id: 10, name: 'Shrimp Scampi', price: 700.00, description: 'Shrimp with garlic butter and white wine reduction on Fetuccine Pastamooth latte with milk', image: 'latte.jpg' },
+    { id: 11, name: 'Lasagna', price: 600.00, description: 'with Meat Sauce and Ricotta Cheese', image: 'latte.jpg' },
+    { id: 12, name: 'Margherita Pizza', price: 800.00, description: 'Basil, Tomato, and Mozzarella', image: 'latte.jpg' },
+    { id: 13, name: 'Chicken Alfredo Pizza', price: 850.00, description: 'Sliced Chicken cutlet with Alfredo Sauce', image: 'latte.jpg' },
+    { id: 14, name: 'Four Cheese Pizza', price: 800.00, description: 'Mozzarella, Cheddar, Blue Cheese, and Monterey Jack', image: 'latte.jpg' },
+    { id: 15, name: 'Meat Lovers Pizza', price: 850.00, description: 'Ground Beef, Salami, Sausage, Pan-Seared Chicken', image: 'latte.jpg' },
+    { id: 16, name: 'Tiramisu', price: 280.00, description: 'Ladyfinger Pastry with Coffee and Cream', image: 'latte.jpg' },
+    { id: 17, name: 'Cheesecake', price: 300.00, description: 'New York Style, Baked with Cream Cheese', image: 'latte.jpg' },
+    { id: 18, name: 'Espresso', price: 200.00, description: 'Bold. Strong. Awooga!', image: 'latte.jpg' },
+    { id: 19, name: 'Cappucino', price: 250.00, description: 'Espresso with Steamed Milk', image: 'latte.jpg' },
+    { id: 20, name: 'Cafelatte', price: 300.00, description: 'Espresso with Milk', image: 'latte.jpg' },
+    { id: 21, name: 'Macchiato', price: 225.00, description: 'Espresso with splash of milk', image: 'latte.jpg' },
+    { id: 22, name: 'Affogato', price: 380.00, description: 'Espresso poured on top of ice cream', image: 'latte.jpg' },
+    { id: 23, name: '24K Gold Affogato', price: 24000.00, description: 'Affogato with 24k Gold Leaf', image: 'latte.jpg' }
   ];
   
   module.exports = products;
